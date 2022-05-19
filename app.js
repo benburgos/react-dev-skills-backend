917397fee9bf2f0ee241d23bd7e4c71c7d237e98
Ben <ben.a.burgos@gmail.com>
2022-05-18 19:29:37 -0700

    adds notes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+// Dependencies
 const express = require('express');
 const logger = require('morgan');
 
 const skillsRouter = require('./routes/skills');
 
+// Create App Object
 const app = express();
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 
+// Route
 app.use('/api/skills', skillsRouter);
 
 module.exports = app;
