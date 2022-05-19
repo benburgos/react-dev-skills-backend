a14fafd0cd725635cc5a74d785fffd3523c3589e
Ben <ben.a.burgos@gmail.com>
2022-05-18 20:11:35 -0700

    refactors code

--- routes/skills.js
+++ routes/skills.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  res.status(200).json([
+    { skill: 'Javascript', level: '5' },
+    { skill: 'HTML', level: '5' },
+    { skill: 'CSS', level: '5' },
+  ]);
 });
 
 module.exports = router;
